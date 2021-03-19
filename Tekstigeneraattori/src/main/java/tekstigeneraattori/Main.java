f87f1252248631e420975479b3dabfde15b68e54
akuivan <akuivan@users.noreply.github.com>
2021-03-19 16:24:47 +0200

    Luo .gitignore ja alusta projekti

---
+++ Tekstigeneraattori/src/main/java/tekstigeneraattori/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tekstigeneraattori;
+
+import tekstigeneraattori.ui.TekstigeneraattoriUi;
+
+/**
+ *
+ * @author heank
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        TekstigeneraattoriUi.main(args);
+    }
+
+}
