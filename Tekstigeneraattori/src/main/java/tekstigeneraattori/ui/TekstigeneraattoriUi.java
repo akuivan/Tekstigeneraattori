7708bb22ea39628963273ef142f2e124acc8923e
akuivan <akuivan@users.noreply.github.com>
2021-03-26 13:11:55 +0200

    TekstigeneraattoriUi -luokan päivitys

--- Tekstigeneraattori/src/main/java/tekstigeneraattori/ui/TekstigeneraattoriUi.java
+++ Tekstigeneraattori/src/main/java/tekstigeneraattori/ui/TekstigeneraattoriUi.java
@@ -3,8 +3,18 @@ package tekstigeneraattori.ui;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * Graafinen käyttöliittymä ja sen käynnistys.
+ *
+ */
 public class TekstigeneraattoriUi extends Application {
 
+    /**
+     * Metodi käynnistää sovelluksen. Myöhemmin tulee käynnistämään graafisen
+     * käyttöliittymän.
+     *
+     * @param args on Main luokan antama muuttuja.
+     */
     public static void main(String[] args) {
         launch(args);
     }
